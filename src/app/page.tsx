31784d029ec2f5b515e1ba92fb75e2f81c80d26d
Ahmad Mohaddeszadeh <ahmad.mohaddeszadieh@konzepthaus-wd.de>
2024-11-22 21:39:57 +0100

    added starter code from superbase docs and installed shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
+import { Button } from "@/components/ui/button";
 
-import { createClient } from '@/utils/superbase/server';
-import { cookies } from 'next/headers';
-
-export default async function Page() {
-  const cookieStore = cookies()
-  const supabase = createClient(cookieStore)
-
-  const { data: todos } = await supabase.from('todos').select()
-
+export default async function Index() {
   return (
-    <ul>
-      {todos?.map((todo) => (
-        <li key={todo.id}>{todo}</li>
-      ))}
-    </ul>
-  )
+    <>
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        <Button>Get started</Button>
+      </main>
+    </>
+  );
 }
